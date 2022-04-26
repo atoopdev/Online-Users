2876ed13300c65d2e3f2d0e01cf29a1a7b14a1ee
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-04-26 16:35:56 -0700

    flexbox for user profile details

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ async function getUser(){
 }
 
 function profileHTML(user){
-console.log(`user's name: ${user.name} username: ${user.username} Company: ${user.company.name}- ${user.company.catchPhrase}, ${user.company.bs} Contact details: Email: ${user.email} phone: ${user.phone} website: ${user.website}`)
+// console.log(`user's name: ${user.name} username: ${user.username} Company: ${user.company.name}- ${user.company.catchPhrase}, ${user.company.bs} Contact details: Email: ${user.email} phone: ${user.phone} website: ${user.website}`)
 
 document.getElementById("user-containerEL").innerHTML = `
 <div class="user-profile">
@@ -27,8 +27,8 @@ document.getElementById("user-containerEL").innerHTML = `
 </div>
 
 <div class="bottom-middle"><h2>Contact Details</h2>
-<div class="contact-details"><span class="email">${user.email}</span> 
-<span class="phone"> ${user.phone}</span> 
+<div class="contact-details"><span class="email">📧${user.email}</span> 
+<span class="phone"> 📱${user.phone}</span> 
 <span class ="website">${user.website}</span>
 </div>
 </div>
