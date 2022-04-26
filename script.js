2e62510c9f72d2a33082475481c4e6f39362011f
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-04-26 13:21:34 -0700

    get user profile and output to screen

---
+++ script.js
@@ -0,0 +1,49 @@
+let profileHTMLOutput = ""
+
+async function getUser(){
+    let response = await fetch(`https://jsonplaceholder.typicode.com/users/3`)
+    let data = await response.json()
+    console.log(data)
+    profileHTML(data)
+}
+
+function profileHTML(user){
+console.log(`user's name: ${user.name} username: ${user.username} Company: ${user.company.name}- ${user.company.catchPhrase}, ${user.company.bs} Contact details: Email: ${user.email} phone: ${user.phone} website: ${user.website}`)
+
+document.getElementById("user-containerEL").innerHTML = `
+<div class="user-profile">
+
+<div class="top"><h2 class="name">${user.name}</h2>
+<span class="username">username: ${user.username}</span>
+</div>
+
+<div class="top-middle"><h2>Company</h2>
+<div class="company">
+<span class="company-name">${user.company.name}</span> 
+<span class="company-phrase>${user.company.catchPhrase}</span> 
+<span class="company-bs">${user.company.bs}</span>
+</div>
+</div>
+
+<div class="bottom-middle"><h2>Contact Details</h2>
+<div class="contact-details"><span class="email">${user.email}</span> 
+<span class="phone"> ${user.phone}</span> 
+<span class ="website">${user.website}</span>
+</div>
+</div>
+
+<div class="profile-footer><h2>Address</h2>
+<div class="user-address">
+<span class="street">${user.address.street}</span>
+<span class="suite">${user.address.suite}</span>
+<span class="city">${user.address.city}</span>
+<span class="zip">${user.address.zip}/span>
+</div>
+</div>
+
+</div>
+
+`
+}
+
+getUser()
